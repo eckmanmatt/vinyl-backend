02b34a04e3487ef9a4fda6654b0970d14f7d6521
Matt Eckman <matteckman@matts-air.lan>
2022-06-28 14:59:52 -0400

    backend complete - awaiting testing

--- src/records/records.module.ts
+++ src/records/records.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { RecordModel } from './schemas/record.schema';
 import { RecordsService } from './records.service';
 import { RecordsController } from './records.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports:[
+    MongooseModule.forFeature([{name: 'Record', schema:RecordModel}])
+  ],
   providers: [RecordsService],
   controllers: [RecordsController]
 })
